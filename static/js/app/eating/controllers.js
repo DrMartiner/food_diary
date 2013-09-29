3bd2a503ee0265b72a37e7dbc1ef66abb788c662
DrMartiner <DrMartiner@GMail.Com>
2013-09-29 12:41:19 +0700

    Showed the eatings who was to be grouped by pub_date

--- static/js/app/eating/controllers.js
+++ static/js/app/eating/controllers.js
@@ -3,34 +3,53 @@
   var EatingController, EatingFoodFormController;
 
   EatingController = angular.module('foodDiaryApp').controller('EatingController', function($scope, EatingResource, EatingFoodResource) {
-    $scope.eatings = [];
+    $scope.allEatings = {};
     $scope.loadEatings = function() {
       return EatingResource.get(function(result) {
-        return $scope.eatings = result.objects;
+        var t;
+        return $scope.allEatings = t = _.groupBy(result.objects, function(obj) {
+          return obj.pub_date.slice(0, 10);
+        });
       });
     };
     $scope.createEating = function() {
       return EatingResource.save({}, function(eating) {
+        var pubDate;
         eating.expand = true;
-        return $scope.eatings.unshift(eating);
+        pubDate = eating.pub_date.slice(0, 10);
+        if (!$scope.allEatings[pubDate]) {
+          $scope.allEatings[pubDate] = [];
+        }
+        return $scope.allEatings[pubDate].unshift(eating);
       });
     };
     $scope.deleteEating = function(eatingId) {
       return EatingResource["delete"]({
         id: eatingId
       }, function() {
-        var eating, index, _i, _len, _ref, _results;
-        _ref = $scope.eatings;
+        var date, eating, eatings, _ref, _results;
+        _ref = $scope.allEatings;
         _results = [];
-        for (_i = 0, _len = _ref.length; _i < _len; _i++) {
-          eating = _ref[_i];
-          if (eating.id === eatingId) {
-            index = $scope.eatings.indexOf(eating);
-            $scope.eatings.splice(index, 1);
-            break;
-          } else {
-            _results.push(void 0);
-          }
+        for (date in _ref) {
+          eatings = _ref[date];
+          _results.push((function() {
+            var _i, _len, _results1;
+            _results1 = [];
+            for (_i = 0, _len = eatings.length; _i < _len; _i++) {
+              eating = eatings[_i];
+              if (eating.id === eatingId) {
+                $scope.allEatings[date] = _.without(eatings, eating);
+                if (!$scope.allEatings[date].length) {
+                  _results1.push(delete $scope.allEatings[date]);
+                } else {
+                  _results1.push(void 0);
+                }
+              } else {
+                _results1.push(void 0);
+              }
+            }
+            return _results1;
+          })());
         }
         return _results;
       });
@@ -39,17 +58,17 @@
       return EatingFoodResource["delete"]({
         id: eatingFoodId
       }, function() {
-        var eating, eatingfood, index, _i, _j, _len, _len1, _ref, _ref1;
-        _ref = $scope.eatings;
-        for (_i = 0, _len = _ref.length; _i < _len; _i++) {
-          eating = _ref[_i];
-          if (eating.id === eatingId) {
+        var date, eating, eatingfood, eatings, _i, _j, _len, _len1, _ref, _ref1;
+        _ref = $scope.allEatings;
+        for (date in _ref) {
+          eatings = _ref[date];
+          for (_i = 0, _len = eatings.length; _i < _len; _i++) {
+            eating = eatings[_i];
             _ref1 = eating.eatingfoods;
             for (_j = 0, _len1 = _ref1.length; _j < _len1; _j++) {
               eatingfood = _ref1[_j];
               if (eatingfood.id === eatingFoodId) {
-                index = eating.eatingfoods.indexOf(eatingfood);
-                eating.eatingfoods.splice(index, 1);
+                _.without(eating.eatingfoods, eatingfood);
                 return;
               }
             }
